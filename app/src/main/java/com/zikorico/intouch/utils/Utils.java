03fa797beb4680488ae22b0a2613c4dd41ab876d
Zak <zaki.aroui@gmail.com>
2018-09-03 23:39:28 -0400

    added Utils class

---
+++ app/src/main/java/com/zikorico/intouch/utils/Utils.java
@@ -0,0 +1,24 @@
+package com.zikorico.intouch.utils;
+
+/**
+ * Created by ikazme on 9/3/18.
+ */
+
+public class Utils {
+
+
+    public static final int EDITOR_REQUEST_CODE = 1001;
+    public static final int NEW_REQUEST_CODE = 1002;
+    public static final int REQUEST_IMAGE_CAPTURE = 1003;
+
+    public static final int PERMISSIONS_REQUEST_READ_CONTACTS = 100;
+    public static final int PERMISSIONS_REQUEST_WRITE_CONTACTS = 102;
+    public static final int PERMISSIONS_REQUEST_WRITE_EXT_STORAGE = 103;
+
+    public int INSERT_CONTACT_REQUEST = 201;
+
+    public static final int EMAIL_QUERY_ID = 0;
+
+
+
+}
